38434f708aadeb852926d9248d5650209f8733c1
Jenner Hanni <jeh.wicker@gmail.com>
2012-12-03 16:35:13 -0800

    Fixed C matrix to be 1 0 instead of 0 1

--- simplesystem.m
+++ simplesystem.m
@@ -10,12 +10,12 @@
 clear all
 
 % Create a simple discrete-time system.
-A_simple = [0 1; -5 -2];
-B_simple = [0; 3];
-C_simple = [0 1];
-D_simple = 0;
+A_simple = [0 1; -5 -2]
+B_simple = [0; 3]
+C_simple = [1 0]
+D_simple = 0
 
-sys_simple = ss(A_simple,B_simple,C_simple,D_simple,0.25);
+sys_simple = ss(A_simple,B_simple,C_simple,D_simple,0.25)
 
-A_eig_simple = eig(sys_simple);
+A_eig_simple = eig(sys_simple)
 
