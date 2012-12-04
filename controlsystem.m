2338c39454e7bc0c7f0953f760deb3077dc442d4
Jenner Hanni <jeh.wicker@gmail.com>
2012-12-03 16:30:25 -0800

    Split into one file for each example system.

--- controlsystem.m
+++ controlsystem.m
@@ -13,16 +13,6 @@
 
 clear all
 
-% Create a simple discrete-time system.
-A_simple = [0 1; -5 -2];
-B_simple = [0; 3];
-C_simple = [0 1];
-D_simple = 0;
-
-sys_simple = ss(A_simple,B_simple,C_simple,D_simple,0.25);
-
-A_eig_simple = eig(A_simple);
-
 % Create Tehrani system, discrete-time system
 % Choose parameters
 
---
+++ simple-system.m
@@ -0,0 +1,21 @@
+% Author: Jenner Hanni
+% Tested in Matlab 2011b on Linux machines. 
+%
+% This file creates a simple, discrete-time system. 
+%
+% Released under the MIT License. 
+% Copyright (c) 2012 Jenner Hanni <jeh.wicker@gmail.com>
+
+
+clear all
+
+% Create a simple discrete-time system.
+A_simple = [0 1; -5 -2];
+B_simple = [0; 3];
+C_simple = [0 1];
+D_simple = 0;
+
+sys_simple = ss(A_simple,B_simple,C_simple,D_simple,0.25);
+
+A_eig_simple = eig(sys_simple);
+
